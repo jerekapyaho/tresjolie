4c7ace939fe2234227e86ef7904cc022dfa30f46
Jere Käpyaho <jere@coniferproductions.com>
2018-03-21 08:53:34 +0200

    Find out which stops are different (WIP)

--- tresjolie/update_firebase_db.py
+++ tresjolie/update_firebase_db.py
@@ -7,6 +7,8 @@ import logging
 import argparse
 import json
 
+from tresjolie import Stop
+
 def read_json_data(json_filename):
     json_data = None
     with open(json_filename) as json_file:
@@ -31,8 +33,9 @@ def get_current_stops():
     stop_ids = list(json_data.keys())
     stops = []
     for s in stop_ids:
-        # print('%s: %s' % (s, json_data[s]))
-        stops.append(json_data[s])
+        new_stop = Stop.from_json(json_data[s])
+        print(new_stop)
+        stops.append(new_stop)
 
     return stops
 
@@ -42,6 +45,13 @@ def test_removal():
     final = [x for x in current if not x in removed]
     print('%s -- %s -- %s' % (current, removed, final))
 
+def test_equal():
+    stop1 = Stop('0001', 'Keskustori M', 61.49751, 23.76151, None, ['4', '4Y'], '837', 'A')
+    stop2 = Stop('0001', 'Keskustori M', 61.49751, 23.76151, None, ['4', '4Y'], '837', 'A')
+    print(stop1 == stop2)
+    stop2 = Stop('0001', 'Keskustori M', 61.49752, 23.76151, None, ['4', '4Y'], '837', 'A')
+    print(stop1 == stop2)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Populate an empty Firebase database from a JSON file.')
     parser.add_argument('-j', '--json', help='Name of JSON data file')
@@ -50,34 +60,48 @@ if __name__ == '__main__':
     current_stops = get_current_stops()
     print('Got %d stops from Firebase' % len(current_stops))
 
-    fresh_stops = read_json_data(args.json)['stops']
+    fresh_stop_data = read_json_data(args.json)['stops']
+    fresh_stops = []
+    for json_data in fresh_stop_data:
+        fresh_stops.append(Stop.from_json(json_data))
     print('Got %d fresh stops from JSON file' % len(fresh_stops))
 
     # Find new stops (stops which are in the fresh stops list but not in current)
-    current_stop_codes = [s['code'] for s in current_stops]
-    fresh_stop_codes = [s['code'] for s in fresh_stops]
-    added_stop_codes = set(current_stop_codes) - set(fresh_stop_codes)
-    print('Added stops: %s' % added_stop_codes)
-
+    current_stop_codes = [s.code for s in current_stops]
+    fresh_stop_codes = [s.code for s in fresh_stops]
     removed_stop_codes = set(fresh_stop_codes) - set(current_stop_codes)
     print('Removed stops: %s' % removed_stop_codes)
+    added_stop_codes = set(current_stop_codes) - set(fresh_stop_codes)
+    print('Added stops: %s' % added_stop_codes)
 
     stops = []
 
     print('Removing stops')
     for s in current_stops:
-        code = s['code']
-        if code in removed_stop_codes:
-            print('DELETE %s' % code)
+        if s.code in removed_stop_codes:
+            print('DELETE %s' % s.code)
         else:
-            print('%s not in current stops' % code)
             stops.append(s)
 
     print('Adding stops')
     for s in fresh_stops:
-        code = s['code']
-        if code in added_stop_codes:
-            print('PUT %s' % code)
+        if s.code in added_stop_codes:
+            print('PUT %s' % s.code)
             stops.append(s)            
-
+    
     print('Final list has %d stops' % len(stops))
+
+    fresh_stops_dict = {}
+    for s in fresh_stops:
+        fresh_stops_dict[s.code] = s
+    #print('Fresh stops in dict: %s' % fresh_stops_dict)
+
+    differing_stops = []
+    for s in stops:
+        fresh_stop = fresh_stops_dict[s.code]
+        if s == fresh_stop:
+            #print('%s OK' % s.code)
+            pass
+        else:
+            differing_stops.append(s)
+    print('Differing stops: %s' % differing_stops)
